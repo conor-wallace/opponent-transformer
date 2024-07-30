f7df2e18c90675b656674f41ce007ad21c41ce47
Conor Wallace <conorw8@gmail.com>
2024-07-30 07:19:59 -0500

    Add args to train script

--- opponent_transformer/scripts/train_smac.sh
+++ opponent_transformer/scripts/train_smac.sh
@@ -6,4 +6,6 @@ exp="single"
 seed=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
-CUDA_VISIBLE_DEVICES=0 python train/train_smac.py
+CUDA_VISIBLE_DEVICES=0 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+--map_name ${map} --seed ${seed} --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 \
+--num_env_steps 10000000 --ppo_epoch 15 --use_value_active_masks --use_eval --eval_episodes 32
